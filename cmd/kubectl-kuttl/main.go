febe32bd98d54b5178f67e47aeb1c77281cd49e7
Ken Sipe <kensipe@gmail.com>
2020-03-23 14:06:55 -0500

    removing kudo reference
    Signed-off-by: Ken Sipe <kensipe@gmail.com>

--- cmd/kubectl-kuttl/main.go
+++ cmd/kubectl-kuttl/main.go
@@ -1,4 +1,4 @@
-// Copyright 2020 KUDO Authors
+// Copyright 2020 KUTTL Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
