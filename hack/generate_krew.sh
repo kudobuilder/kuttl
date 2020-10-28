a75db25c6fc812370b47e9b54e3b01dfe918f657
Ken Sipe <kensipe@gmail.com>
2020-10-28 16:56:05 -0500

    adding in artifact detection and failure if not
    Signed-off-by: Ken Sipe <kensipe@gmail.com>

--- hack/generate_krew.sh
+++ hack/generate_krew.sh
@@ -4,6 +4,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+declare -r TRUE=0
+declare -r FALSE=1
+
 # This script generates a Krew-compatible plugin manifest. It should be run after goreleaser.
 
 VERSION=${VERSION:-$(git describe --tags | sed 's/^v//g')}
@@ -17,12 +20,19 @@ function generate_platform {
         ARCH=i386
     fi
 
+    URL="https://github.com/kudobuilder/kuttl/releases/download/v${VERSION}/kuttl_${VERSION}_${1}_${ARCH}.tar.gz"
+    # check file exists first!
+    if ! curl --output /dev/null --silent --head --fail "$URL"; then
+      >&2 echo "URL does not exist: $URL"
+      return $FALSE
+    fi
+
     local sha
     PLATFORM=$(uname)
     if [ "$PLATFORM" == 'Darwin' ]; then
-      sha=$(curl -L https://github.com/kudobuilder/kuttl/releases/download/v"${VERSION}"/kuttl_"${VERSION}"_"${1}"_"${ARCH}".tar.gz | shasum -a 256 - | awk '{print $1}')
+      sha=$(curl -L "$URL" | shasum -a 256 - | awk '{print $1}')
     else
-      sha=$(curl -L https://github.com/kudobuilder/kuttl/releases/download/v"${VERSION}"/kuttl_"${VERSION}"_"${1}"_"${ARCH}".tar.gz | sha256sum - | awk '{print $1}')
+      sha=$(curl -L "$URL" | sha256sum - | awk '{print $1}')
     fi
 
     cat <<EOF
@@ -34,6 +44,7 @@ function generate_platform {
     sha256: "${sha}"
     bin: "${3}"
 EOF
+return $TRUE
 }
 
 rm -f kuttl.yaml
@@ -59,8 +70,12 @@ generate_platform linux amd64 ./kubectl-kuttl >> kuttl.yaml
 generate_platform linux 386 ./kubectl-kuttl >> kuttl.yaml
 generate_platform darwin amd64 ./kubectl-kuttl >> kuttl.yaml
 
+### Discontinued support for 32-bit darwin
+# generate_platform darwin 386 ./kubectl-kuttl >> kuttl.yaml
+
 ### KUTTL is not currently built for Windows. Uncomment once it is.
 # generate_platform windows amd64 ./kubectl-kuttl.exe >> kuttl.yaml
 # generate_platform windows 386 ./kubectl-kuttl.exe >> kuttl.yaml
 
+echo "Successful!"
 echo "To publish to the krew index, create a pull request to https://github.com/kubernetes-sigs/krew-index/tree/master/plugins to update kuttl.yaml with the newly generated kuttl.yaml."
