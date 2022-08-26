22fcf0bd08403d105a289e7030eff72a815f7fa8
Ken Sipe <kensipe@gmail.com>
2022-08-25 22:31:46 -0500

    Replaced deprecated call in APIServer testenv
    Signed-off-by: Ken Sipe <kensipe@gmail.com>

--- pkg/test/harness.go
+++ pkg/test/harness.go
@@ -219,7 +219,7 @@ func (h *Harness) RunTestEnv() (*rest.Config, error) {
 
 	h.T.Logf("started test environment (kube-apiserver and etcd) in %v, with following options:\n%s",
 		time.Since(started),
-		strings.Join(testenv.Environment.ControlPlane.GetAPIServer().Args, "\n"))
+		strings.Join(testenv.Environment.ControlPlane.GetAPIServer().Configure().AsStrings(nil), "\n"))
 	h.env = testenv.Environment
 
 	return testenv.Config, nil
