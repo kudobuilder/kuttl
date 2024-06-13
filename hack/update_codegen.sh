ac18be149505f281873153221e6bb1d5c2eb0ea6
Marcin Owsiany <porridge@redhat.com>
2024-06-13 20:25:17 +0200

    Bump dependencies and group them. (#529)
    Signed-off-by: Marcin Owsiany <porridge@redhat.com>
    Co-authored-by: Moritz Clasmeier <111092021+mclasmeier@users.noreply.github.com>

--- hack/update_codegen.sh
+++ hack/update_codegen.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Based on https://github.com/kubernetes/code-generator/blob/master/examples/hack/update-codegen.sh
 
 set -o errexit
 set -o nounset
@@ -8,11 +9,12 @@ set -o pipefail
 # borrowed and modified from https://github.com/heptio/contour/pull/1010.
 # it has been modified to enable caching.
 export GO111MODULE=on
-version_and_repo=$(go list -f '{{if .Replace}}{{.Replace.Version}} {{.Replace.Path}}{{else}}{{.Version}} {{.Path}}{{end}}' -m k8s.io/code-generator)
-VERSION="$(echo "${version_and_repo}" | cut -d' ' -f 1 | rev | cut -d"-" -f1 | rev)"
-CODEGEN_REPO="$(echo "${version_and_repo}" | cut -d' ' -f 2)"
+VERSION="$(go list -f '{{if .Replace}}{{.Replace.Version}}{{else}}{{.Version}}{{end}}' -m k8s.io/code-generator | rev | cut -d"-" -f1 | rev)"
 REPO_ROOT="${REPO_ROOT:-$(git rev-parse --show-toplevel)}"
-CODE_GEN_DIR="${REPO_ROOT}/hack/code-gen/$VERSION"
+CODE_GEN_DIR="${REPO_ROOT}/hack/vendor/code-gen/$VERSION"
+
+# Cleanup cached dirs at old path which will interfere with the recursive grep that gen_helpers does.
+rm -rf "${REPO_ROOT}/hack/code-gen/"
 
 if [[ -d ${CODE_GEN_DIR} ]]; then
     echo "Using cached code generator version: $VERSION"
@@ -21,18 +23,10 @@ else
     git -C "${CODE_GEN_DIR}" reset --hard "${VERSION}"
 fi
 
-# Fake being in a $GOPATH until kubernetes fully supports modules
-# https://github.com/kudobuilder/kudo/issues/1252
-FAKE_GOPATH="$(mktemp -d)"
-trap 'chmod -R u+rwX ${FAKE_GOPATH} && rm -rf ${FAKE_GOPATH}' EXIT
-FAKE_REPOPATH="${FAKE_GOPATH}/src/github.com/kudobuilder/kuttl"
-mkdir -p "$(dirname "${FAKE_REPOPATH}")" && ln -s "${REPO_ROOT}" "${FAKE_REPOPATH}"
-export GOPATH="${FAKE_GOPATH}"
-cd "${FAKE_REPOPATH}"
+# Set GOBIN to make gen_helpers install and run binaries in the versioned directory.
+export GOBIN="${CODE_GEN_DIR}/bin"
 
-"${CODE_GEN_DIR}"/generate-groups.sh \
-  deepcopy \
-  github.com/kudobuilder/kuttl/pkg/client \
-  github.com/kudobuilder/kuttl/pkg/apis \
-  "testharness:v1beta1" \
-  --go-header-file hack/boilerplate.go.txt # must be last for some reason
+source "${CODE_GEN_DIR}/kube_codegen.sh"
+kube::codegen::gen_helpers \
+    --boilerplate "${REPO_ROOT}/hack/boilerplate.go.txt" \
+    "${REPO_ROOT}"
