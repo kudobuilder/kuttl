c6c3e8a333472499a25442614adb16ced01cc7ca
Kumar Mallikarjuna <kumarmallikarjuna.work@gmail.com>
2024-12-14 12:02:03 +0530

    chore: incorporate review comments
    Signed-off-by: Kumar Mallikarjuna <kumarmallikarjuna.work@gmail.com>

--- pkg/test/expression_integration_test.go
+++ pkg/test/expression_integration_test.go
@@ -17,7 +17,7 @@ import (
 	testutils "github.com/kudobuilder/kuttl/pkg/test/utils"
 )
 
-func TestAssertExpressions(t *testing.T) {
+func buildTestStep(t *testing.T, tcName string) *Step {
 	codednsDeployment := &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "coredns",
@@ -34,24 +34,24 @@ func TestAssertExpressions(t *testing.T) {
 		},
 	}
 
-	buildTestStep := func(tcName string) *Step {
-		return &Step{
-			Name:   t.Name(),
-			Index:  0,
-			Logger: testutils.NewTestLogger(t, tcName),
-			Client: func(bool) (client.Client, error) {
-				return fake.
-					NewClientBuilder().
-					WithObjects(codednsDeployment, metricServerPod).
-					WithScheme(testutils.Scheme()).
-					Build(), nil
-			},
-			DiscoveryClient: func() (discovery.DiscoveryInterface, error) {
-				return testutils.FakeDiscoveryClient(), nil
-			},
-		}
+	return &Step{
+		Name:   t.Name(),
+		Index:  0,
+		Logger: testutils.NewTestLogger(t, tcName),
+		Client: func(bool) (client.Client, error) {
+			return fake.
+				NewClientBuilder().
+				WithObjects(codednsDeployment, metricServerPod).
+				WithScheme(testutils.Scheme()).
+				Build(), nil
+		},
+		DiscoveryClient: func() (discovery.DiscoveryInterface, error) {
+			return testutils.FakeDiscoveryClient(), nil
+		},
 	}
+}
 
+func TestAssertExpressions(t *testing.T) {
 	testCases := []struct {
 		name          string
 		loadingFailed bool
@@ -91,7 +91,7 @@ func TestAssertExpressions(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			step := buildTestStep(tc.name)
+			step := buildTestStep(t, tc.name)
 
 			fName := fmt.Sprintf(
 				"step_integration_test_data/assert_expressions/%s/00-assert.yaml",
