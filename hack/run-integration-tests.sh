593c75916f8942864580661c45bc327503ec28f2
Marcin Owsiany <porridge@redhat.com>
2025-10-27 09:06:01 +0100

    chore: run tests in non-verbose mode by default (#636)
    Signed-off-by: Marcin Owsiany <porridge@redhat.com>

--- hack/run-integration-tests.sh
+++ hack/run-integration-tests.sh
@@ -14,8 +14,8 @@ then
     go get github.com/jstemmer/go-junit-report
     go install github.com/jstemmer/go-junit-report
     go mod tidy
-    go test -tags integration ./pkg/... ./internal/... -v -mod=readonly -coverprofile cover-integration.out 2>&1 |tee /dev/fd/2 |go-junit-report -set-exit-code > reports/integration_report.xml
+    go test -tags integration ./pkg/... ./internal/... -mod=readonly -coverprofile cover-integration.out "$@" 2>&1 |tee /dev/fd/2 |go-junit-report -set-exit-code > reports/integration_report.xml
 else
     echo "Running integration tests without junit output"
-    go test -tags integration ./pkg/... ./internal/... -v -mod=readonly -coverprofile cover-integration.out
+    go test -tags integration ./pkg/... ./internal/... -mod=readonly -coverprofile cover-integration.out "$@"
 fi
