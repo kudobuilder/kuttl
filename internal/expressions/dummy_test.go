5804c4fa1f1fad0419d490202dd267edf0f8d136
Marcin Owsiany <porridge@redhat.com>
2026-01-05 07:16:46 +0100

    chore: add dummy tests to avoid go bug
    https://github.com/golang/go/issues/75031
    
    Signed-off-by: Marcin Owsiany <porridge@redhat.com>

---
+++ internal/expressions/dummy_test.go
@@ -0,0 +1,9 @@
+package expressions
+
+import "testing"
+
+// TestDummy is a no-op test to satisfy coverage tools
+func TestDummy(t *testing.T) {
+	// This test exists only to ensure the package is recognized by go test
+	// and to avoid "go: no such tool 'covdata'" warnings
+}
